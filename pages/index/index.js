a4648e8c99814d460190d0911175e6fc5ebeb492
shenhuanjie <shenhuanjie@live.cn>
2018-11-28 19:19:58 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,23 @@
+Page({
+  data: {
+    imgUrls: [
+      '/resource/images/img_swiper_01.png',
+      '/resource/images/img_swiper_01.png',
+      '/resource/images/img_swiper_01.png',
+      '/resource/images/img_swiper_01.png',
+      '/resource/images/img_swiper_01.png'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  /**
+   * 跳转站点列表页面
+   */
+  toSiteList: function() {
+    wx.navigateTo({
+      url: '../../pages/sites/sites'
+    })
+  }
+})
