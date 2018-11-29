003349f9ce00ccbf5a97395085b2092307551911
shenhuanjie <shenhuanjie@live.cn>
2018-11-29 21:26:45 +0800

    增加登录页面的静态跳转方法

--- pages/login/login.js
+++ pages/login/login.js
@@ -11,62 +11,83 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
+
+  /**
+   * 用户点击登录按钮
+   */
+  onLogin: function() {
+    console.log("onLogin");
+    // TODO: 实现登录逻辑
+    var _that = this;
+    _that.toIndex();
+  },
+
+  /**
+   * 跳转平台首页
+   */
+  toIndex() {
+    console.log("onLogin");
+    wx.switchTab({
+      url: '../../pages/index/index',
+    })
+  },
+
   /**
    * 跳转注册页面
    */
-  toRegister:function(){
+  toRegister: function() {
     wx.navigateTo({
       url: '../../pages/register/register',
     })
