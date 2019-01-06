89f1d2fd488c16bf8cb3d3971f00dcb9c1848bee
SHEN.HUANJIE <shenhuanjie@live.cn>
2019-01-06 14:53:44 +0800

    commit

--- pages/complaint/complaint.js
+++ pages/complaint/complaint.js
@@ -1,66 +1,99 @@
-// pages/complaint/complaint.js
+const app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        ddlType: ["品质", "损坏", "少货", "其他"],
+        index: -1,
+        dataTitle: "",
+        dataMsg: ""
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
+    bindTitle: function(event) {
+        this.setData({
+            dataTitle: event.detail.value
+        });
+    },
+    bindMsg: function(event) {
+        this.setData({
+            dataMsg: event.detail.value
+        });
+    },
+    bindPickerChange: function(even) {
+        console.log(even);
+        var index = parseInt(even.detail.value);
+        this.setData({
+            index: index
+        })
+    },
+    bindSubmit: function(even) {
+        var that = this;
+        var index = this.data.index;
+        var dataType = this.data.ddlType[index];
+        var dataTitle = this.data.dataTitle;
+        var dataMsg = this.data.dataMsg;
 
-  },
+        var checkFlat = true;
+        var errorMsg = "";
+        if (dataTitle == "") {
+            errorMsg = "请输入主题";
+            checkFlat = false;
+        } else if (checkFlat && dataType == -1) {
+            errorMsg = "请选择类型";
+            checkFlat = false;
+        } else if (checkFlat && dataMsg == "") {
+            errorMsg = "请输入投诉建议内容";
+            checkFlat = false;
+        }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+        if (!checkFlat) {
+            wx.showToast({
+                title: errorMsg,
+                icon: 'none',
+                duration: 2000
+            })
+            return false;
+        }
+        that.doSubmit();
+    },
+    doSubmit: function() {
+        var that = this;
+        var url = app.globalData.appUrl + "/Complain/Save";
+        var data = {
+            memCode: wx.getStorageSync("memcode"),
+            title: that.data.dataTitle,
+            message: that.data.dataMsg,
+            cType: that.data.dataType,
+            // pic: null
+        }
+        console.log(data);
+        wx.request({
+            url: url,
+            data: data,
+            method: "POST",
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success(res) {
+                var data = res.data;
+                var Msg = data.Msg;
+                var Success = data.Success;
+                console.log(data);
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+                wx.showToast({
+                    title: Msg,
+                    icon: 'none',
+                    duration: 2000
+                })
+            }
+        })
+    }
 })
