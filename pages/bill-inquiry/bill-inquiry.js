ca85f29e921364b6a46fe33fe88a94a2a219c9ca
SHEN.HUANJIE <shenhuanjie@live.cn>
2019-01-06 12:44:48 +0800

    update
    1、修改 密码
    2、订单查询
    3、账单查询
    4、物流查询

--- pages/bill-inquiry/bill-inquiry.js
+++ pages/bill-inquiry/bill-inquiry.js
@@ -1,5 +1,5 @@
 const app = getApp();
-
+// pages/order-inquiry/order-inquiry.js
 Page({
 
     /**
@@ -8,15 +8,55 @@ Page({
     data: {
         dateStart: app.formatDate((new Date()), "yyyy-MM-dd"),
         dateEnd: app.formatDate((new Date()), "yyyy-MM-dd"),
+        dataTotal: 0,
+        dataList: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
-
+    onLoad: function (options) {
+        this.getOrderListByUserCode();
     },
-    bindDateChange: function(e) {
+    /**
+     * 获取订单查询列表
+     */
+    getOrderListByUserCode() {
+        var that = this;
+        var url = app.globalData.appUrl + "/Payrecord/GetListByUserCode";
+        var data = {
+            startTime: that.data.dateStart,
+            endTime: that.data.dateEnd,
+            memCode: wx.getStorageSync("memcode"),
+            pageSize: 1000
+        }
+        console.log(data);
+        wx.request({
+            url: url,
+            data: data,
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success(res) {
+                var data = res.data;
+                console.log(data);
+                that.setData({
+                    dataTotal: data.Total,
+                    dataList: data.Rows
+                })
+            }
+        })
+    },
+    /**
+     * 绑定查询按钮
+     */
+    bindSearch: function (e) {
+        this.getOrderListByUserCode();
+    },
+    /**
+     * 绑定日期选择器事件
+     */
+    bindDateChange: function (e) {
         var that = this;
         console.log(e);
         console.log('picker发送选择改变，携带值为', e.detail.value)
@@ -47,6 +87,5 @@ Page({
                 dateEnd: dateEnd
             })
         }
-
     },
 })
--- pages/password-revise/password-revise.js
+++ pages/password-revise/password-revise.js
@@ -1,66 +1,124 @@
-// pages/password-revise/password-revise.js
+const app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        password_old: "",
+        password_old_: "",
+        password_new: ""
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.getMemberByCode();
+    },
+    bindPassword: function(event) {
+        var id = event.target.id;
+        var value = event.detail.value;
+        var password_old = this.data.password_old;
+        var password_old_ = this.data.password_old_;
+        var password_new = this.data.password_new;
+        if (id == "password_old") {
+            password_old = value;
+        } else if (id == "password_old_") {
+            password_old_ = value;
+        } else if (id == "password_new") {
+            password_new = value;
+        }
+        this.setData({
+            password_old: password_old,
+            password_old_: password_old_,
+            password_new: password_new
+        });
+    },
+    bindSubmit: function() {
+        var that = this;
+        var password_old = this.data.password_old;
+        var password_old_ = this.data.password_old_;
+        var password_new = this.data.password_new;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+        var checkFlat = true;
+        var errorMsg = "";
+        if (password_old == "") {
+            errorMsg = "请输入旧密码";
+            checkFlat = false;
+        } else if (checkFlat && password_old_ == "") {
+            errorMsg = "请在输入旧密码";
+            checkFlat = false;
+        } else if (checkFlat && password_new == "") {
+            errorMsg = "请输入新密码";
+            checkFlat = false;
+        } else if (checkFlat && password_old != password_old_) {
+            errorMsg = "两次密码输入不一致";
+            checkFlat = false;
+        }
+        if (!checkFlat) {
+            wx.showToast({
+                title: errorMsg,
+                icon: 'none',
+                duration: 2000
+            })
+            return false;
+        }
+        that.doChangePwd();
+    },
+    doChangePwd: function() {
+        var that = this;
+        var url = app.globalData.appUrl + "/Member/ChangePwd";
+        var data = {
+            memCode: wx.getStorageSync("memcode"),
+            oldPwd: that.data.password_old,
+            newPwd: that.data.password_new
+        }
+        console.log(data);
+        wx.request({
+            url: url,
+            data: data,
+            method: "POST",
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success(res) {
+                var data = res.data;
+                var Msg = data.Msg;
+                var Success = data.Success;
+                console.log(data);
+                
+                wx.showToast({
+                    title: Msg,
+                    icon: 'none',
+                    duration: 2000
+                })
+            }
+        })
+    },
+    /**
+     * 获取用户信息
+     */
+    getMemberByCode: function() {
+        var that = this;
+        var url = app.globalData.appUrl + "/Member/GetMemberByCode";
+        var data = {
+            memCode: wx.getStorageSync("memcode"),
+        }
+        console.log(data);
+        wx.request({
+            url: url,
+            data: data,
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success(res) {
+                var data = res.data;
+                console.log(data);
+                that.setData({
+                    member: data.Member
+                })
+            }
+        })
+    }
 })
